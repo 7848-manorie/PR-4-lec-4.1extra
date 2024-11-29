005636d8c942873cd9dda885190ce3dfdba9d8d7
7848-manorie <166717416+7848-manorie@users.noreply.github.com>
2024-11-29 18:48:38 +0530

    Update angle.c

--- angle.c
+++ angle.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 void main(){
     float a=90,b,c;
-    printf("First angle is:");
-    scanf("%f",&a);
-    printf("Second angle is:");
+    printf("First angle is: 90\n");
+    printf("enter angle b:");
     scanf("%f",&b);
-
     c=180-(a+b);
 
-    printf("Third angle is:%.2f",c);
+    if(b>=0 && b<=90){
+        printf("Third angle is:%.2f",c);
+    }
+    else{
+         printf("invalid angle");
+    }
 }
